3bfcf26ebd722eae64cdd6dad268a7c2892d1571
inomata137 <inomata2016@icloud.com>
2025-02-16 02:33:28 +0900

    unify moveaxis implementations

--- src/axis.rs
+++ src/axis.rs
@@ -1,12 +1,12 @@
-pub fn moveaxis_dyn<S, const NMOVE: usize>(
-    a: ndarray::ArrayBase<S, ndarray::IxDyn>,
+pub fn moveaxis<S, I, const NMOVE: usize>(
+    a: ndarray::ArrayBase<S, ndarray::Dim<I>>,
     source: &[usize; NMOVE],
     destination: &[usize; NMOVE],
-) -> ndarray::ArrayBase<S, ndarray::Dim<ndarray::IxDynImpl>>
+) -> ndarray::ArrayBase<S, ndarray::Dim<I>>
 where
     S: ndarray::Data,
-    ndarray::IxDynImpl: ndarray::IntoDimension<Dim = ndarray::IxDyn>,
-    ndarray::IxDyn: ndarray::Dimension,
+    I: ndarray::IntoDimension<Dim = ndarray::Dim<I>> + TryFrom<Vec<usize>, Error: std::fmt::Debug>,
+    ndarray::Dim<I>: ndarray::Dimension,
 {
     let ndim = a.ndim();
     let dst_src = std::collections::BTreeMap::from_iter(std::iter::zip(destination, source));
@@ -14,49 +14,23 @@ where
     source.sort();
     let mut rest = (0..ndim).filter(|&i| source.binary_search(&i).is_err());
 
-    let axes = (0..ndim)
-        .map(|dst| {
-            if let Some(&&src) = dst_src.get(&dst) {
-                src
-            } else {
-                rest.next().unwrap()
-            }
-        })
-        .collect::<Vec<_>>();
-
-    a.permuted_axes(axes)
-}
-
-pub fn moveaxis_static<S, const NDIM: usize, const NMOVE: usize>(
-    a: ndarray::ArrayBase<S, ndarray::Dim<[usize; NDIM]>>,
-    source: &[usize; NMOVE],
-    destination: &[usize; NMOVE],
-) -> ndarray::ArrayBase<S, ndarray::Dim<[usize; NDIM]>>
-where
-    S: ndarray::Data,
-    [usize; NDIM]: ndarray::IntoDimension<Dim = ndarray::Dim<[usize; NDIM]>>,
-    ndarray::Dim<[usize; NDIM]>: ndarray::Dimension,
-{
-    let dst_src = std::collections::BTreeMap::from_iter(std::iter::zip(destination, source));
-    let mut source = *source;
-    source.sort();
-    let mut rest = (0..NDIM).filter(|&i| source.binary_search(&i).is_err());
-
-    let mut axes = [0; NDIM];
-    for (dst, x) in axes.iter_mut().enumerate() {
-        *x = if let Some(&&src) = dst_src.get(&dst) {
+    let mut axes = Vec::with_capacity(ndim);
+    for dst in 0..ndim {
+        let axis_idx = if let Some(&&src) = dst_src.get(&dst) {
             src
         } else {
             rest.next().unwrap()
-        }
+        };
+        axes.push(axis_idx);
     }
 
+    let axes = I::try_from(axes).unwrap();
     a.permuted_axes(axes)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{moveaxis_dyn, moveaxis_static};
+    use super::moveaxis;
 
     #[test]
     fn test_moveaxis_dyn() {
@@ -67,7 +41,10 @@ mod tests {
             .into_shape_with_order(ndarray::IxDyn(&[2, 3, 4, 5]))
             .unwrap();
 
-        let arr_4x2x3x5 = moveaxis_dyn(arr_2x3x4x5, &[2], &[0]);
+        let arr_4x2x3x5 = moveaxis(arr_2x3x4x5, &[2], &[0]);
+
+        ndarray::IntoDimension::into_dimension([1, 2, 3, 4]);
+        let _: [usize; 4] = TryFrom::try_from(vec![1, 2, 3, 4]).unwrap();
         assert_eq!(arr_4x2x3x5.shape(), [4, 2, 3, 5]);
     }
 
@@ -78,7 +55,7 @@ mod tests {
                 .into_shape_with_order(ndarray::Ix4(2, 3, 4, 5))
                 .unwrap();
 
-        let arr_4x2x3x5 = moveaxis_static(arr_2x3x4x5, &[2], &[0]);
+        let arr_4x2x3x5 = moveaxis(arr_2x3x4x5, &[2], &[0]);
         assert_eq!(arr_4x2x3x5.shape(), [4, 2, 3, 5]);
     }
 }
